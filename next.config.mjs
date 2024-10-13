3d5ac3a70448f0ec15046c19f500dd8ce9df8dc9
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2024-10-13 12:53:39 +0530

    Hotfix vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -9,27 +9,8 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-            {
-                // Matching all API routes
-                source: '/api/(.*)',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store, max-age=0, must-revalidate',
-                    },
-                    {
-                        key: 'CDN-Cache-Control',
-                        value: 'no-store',
-                    },
-                    {
-                        key: 'Vercel-CDN-Cache-Control',
-                        value: 'no-store',
-                    },
-                ],
-            },
-        ];
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
     },
 };
 
